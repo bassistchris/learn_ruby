dc142bc6f05d19bf4d1a852c0e9d7c16e58277df
bassistchris <christopher.lutz91@gmail.com>
2019-02-05 19:28:42 +0000

    complete learn_ruby

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,23 @@
 class Book
-# write your code here
+
+  attr_reader :title
+    
+  def title=(str)
+    strArray = str.split(" ")
+    ignore = ["a", "an", "the", "in", "and", "to", "of"]
+    finalWords = [(strArray[0].capitalize)]
+
+    transWords = ''
+    strArray.each_with_index do |word, i|
+      next if i == 0
+      if ignore.include? word
+        transWord = strArray[i]
+        finalWords.push(transWord)
+      else
+        transWord = strArray[i].capitalize
+        finalWords.push(transWord)
+      end
+    end
+    @title = finalWords.join(" ")
+  end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,26 @@
 class Timer
-  #write your code here
+  
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hour = 0
+    min = 0
+    sec = 0
+    if @seconds / 3600 > 0
+      hour = @seconds / 3600
+      min = (@seconds % 3600) / 60
+      sec = (@seconds % 3600) % 60
+    elsif @seconds / 60 > 0
+      hour = 0
+      min = (@seconds / 60)
+      sec = (@seconds % 60)
+    else
+      sec = @seconds
+    end
+    @time_string = sprintf("%02d:%02d:%02d", hour, min, sec)
+  end
 end
